723cb139d8bb8a45655d3b62319ea84aa7cb191a
unknown <mejiapazalfredo@gmail.com>
2023-10-05 17:50:43 -0600

    Added new subject ES2015 - with material on const & let - with its respective exercise

---
+++ ES2015/let&const/app.js
@@ -0,0 +1,49 @@
+function sing() {
+    if(true) {
+        var song = "Goodbye Yellow Brick Road";
+        var song = 234;
+    }
+    console.log(song);
+}
+
+if(true) {
+    let color = "teal";
+    console.log(color);
+    if(true) {
+        console.log(color);
+    }
+}
+
+function dance() {
+    let danceType = "tango";
+    console.log(danceType);
+}
+
+var i = "hello";
+for(let i = 0; i<=3; i++) {
+    console.log(i);
+}
+console.log(i);
+
+
+let mood = "fatastic";
+function getMood(num) {
+    let mood = "meh";
+    if(num < 3) {
+        let mood = "bad";
+        console.log(mood);
+    }
+
+    return mood;
+}
+
+console.log(getMood(2));
+
+
+const pi = 3.1416;
+if(true) {
+    const pi = 4.516;
+    console.log(pi);
+}
+
+console.log(pi);
