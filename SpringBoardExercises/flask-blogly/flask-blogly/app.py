fe7ce161b234f65348d9d7eabb9f1162ae817504
Coffee6ean <mejiapazalfredo@gmail.com>
2023-11-28 13:33:29 -0600

    Added 'SQLAlchemy'excersises to the repo

--- SpringBoardExercises/flask-blogly/flask-blogly/app.py
+++ SpringBoardExercises/flask-blogly/flask-blogly/app.py
@@ -1,7 +1,7 @@
 """Blogly application."""
 
-from flask import Flask
-from models import db, connect_db
+from flask import Flask, redirect, render_template
+from models import db, connect_db, User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
@@ -10,3 +10,74 @@ app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
 db.create_all()
+
+@app.route("/")
+def home_page():
+    """List users"""
+
+    return redirect("/users")
+
+@app.route("/users")
+def users_index():
+    """Show page with all users available"""
+
+    users = User.query.order_by(User.last_name, User.first_name).all()
+    return render_template("users/index.html", users=users)
+
+@app.route('/users/new', methods=["GET"])
+def users_new_form():
+    """Show a form to create a new user"""
+
+    return render_template('users/new.html')
+
+@app.route("/users/new", methods=["POST"])
+def users_new():
+    """Handle form submission for creating a new user"""
+
+    new_user = User(
+        first_name=request.form['first_name'],
+        last_name=request.form['last_name'],
+        image_url=request.form['image_url'] or None)
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect("/users")
+
+@app.route('/users/<int:user_id>')
+def users_show(user_id):
+    """Show a page with info on a specific user"""
+
+    user = User.query.get_or_404(user_id)
+    return render_template('users/show.html', user=user)
+
+@app.route('/users/<int:user_id>/edit')
+def users_edit(user_id):
+    """Show a form to edit an existing user"""
+
+    user = User.query.get_or_404(user_id)
+    return render_template('users/edit.html', user=user)
+
+@app.route('/users/<int:user_id>/edit', methods=["POST"])
+def users_update(user_id):
+    """Handle form submission for updating an existing user"""
+
+    user = User.query.get_or_404(user_id)
+    user.first_name = request.form['first_name']
+    user.last_name = request.form['last_name']
+    user.image_url = request.form['image_url']
+
+    db.session.add(user)
+    db.session.commit()
+
+    return redirect("/users")
+
+@app.route('/users/<int:user_id>/delete', methods=["POST"])
+def users_destroy(user_id):
+    """Handle form submission for deleting an existing user"""
+
+    user = User.query.get_or_404(user_id)
+    db.session.delete(user)
+    db.session.commit()
+
+    return redirect("/users")
--- SpringBoardExercises/flask-blogly/flask-blogly/models.py
+++ SpringBoardExercises/flask-blogly/flask-blogly/models.py
@@ -1,1 +1,36 @@
 """Models for Blogly."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+DEFAULT_IMAGE_URL = "https://www.freeiconspng.com/uploads/icon-user-blue-symbol-people-person-generic--public-domain--21.png"
+
+def connect_db(app):
+    """Connect to database"""
+
+    db.app = app
+    db.init_app(app)
+
+class User(db.Model):
+    """Users."""
+
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, 
+                   primary_key=True, 
+                   autoincrements=True)
+    first_name = db.Column(db.String(20), 
+                           nullable=False, 
+                           unique=True)
+    last_name = db.Column(db.String(20), 
+                          nullable=False)
+    image_url = db.Column(db.String, 
+                          nullable=False, 
+                          default=DEFAULT_IMAGE_URL)
+    
+    @property
+    def full_name(self):
+        """Return full name of user."""
+
+        return f"{self.first_name} {self.last_name}"
