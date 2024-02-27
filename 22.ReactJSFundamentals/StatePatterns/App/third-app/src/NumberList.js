f03171aebfa12e20ba38d25bf0593c3cf20917b3
Coffee6ean <mejiapazalfredo@gmail.com>
2024-02-27 11:23:57 -0600

    Adding unique identifier key to each element - Randomly generated (Not Recommended)

--- 22.ReactJSFundamentals/StatePatterns/App/third-app/src/NumberList.js
+++ 22.ReactJSFundamentals/StatePatterns/App/third-app/src/NumberList.js
@@ -1,8 +1,12 @@
 import React, {useState} from 'react';
 import NumberItem from './NumberItem';
 
+const genId = () => {
+    return Math.random() * 100000000;
+}
+
 const NumberList = () => {
-    const [numbers, setNumbers] = useState([2,5,7,11,12,18]);
+    const [numbers, setNumbers] = useState([2,5,7,11,12,18,11,11]);
     const remove = (num) => {
         setNumbers(numbers.filter(n => n !== num));
         console.log("Removing:", num);
@@ -11,7 +15,7 @@ const NumberList = () => {
     return (
         <ul>
             {numbers.map(n => (
-                <NumberItem number={n} remove={remove}/>
+                <NumberItem number={n} remove={remove} key={genId()}/>
             ))}
         </ul>
     )
