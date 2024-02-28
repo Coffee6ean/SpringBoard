3203d66322ccb0c15b8c330a03cca77f1a198f67
Coffee6ean <mejiapazalfredo@gmail.com>
2024-02-28 11:42:09 -0600

    Adding validators to forms in 'ReactJSFundamentals/Fomrs'

--- 22.ReactJSFundamentals/Forms/App/fifth-app/src/UserForm.js
+++ 22.ReactJSFundamentals/Forms/App/fifth-app/src/UserForm.js
@@ -8,9 +8,17 @@ const UserForm = () => {
     }
 
     const [formData, setFormData] = useState(initialState);
-
+    const [isInvalid, setIsInvalid] = useState(true);
+    const [isTouched, setIsTouched] = useState(false)
     const handleChange = (e) => {
         const {name, value} = e.target;
+        setIsTouched(true);
+        if(value === '') {
+            setIsInvalid(true);
+        } else {
+            setIsInvalid(false);
+        }
+
         setFormData(data => ({
             ...data,
             [name]: value
@@ -19,9 +27,11 @@ const UserForm = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        const {username, email, password} = formData;
-        alert(`Created user, ${username} w/email ${email} & password ${password}`);
-        setFormData(initialState);
+        const {email} = formData;
+        if(isInvalid) {
+            alert(`Added you to mailing list, ${email}`);
+            setFormData(initialState);
+        }
     }
 
     return (
@@ -55,6 +65,7 @@ const UserForm = () => {
                 value={formData.password} 
                 onChange={handleChange}
             />
+            {isInvalid && isTouched && <span style={{color: 'red'}}>Email cannot be blank!</span>}
             <button>Add me to list!</button>
         </form>
     )
