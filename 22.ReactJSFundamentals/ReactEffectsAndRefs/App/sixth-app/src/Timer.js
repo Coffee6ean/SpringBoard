72b8575e0e799acf32891f15dd809bf763431ad4
Coffee6ean <mejiapazalfredo@gmail.com>
2024-02-28 12:59:13 -0600

    Adding 'Intro to useEffect' to the repo

---
+++ 22.ReactJSFundamentals/ReactEffectsAndRefs/App/sixth-app/src/Timer.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+
+const Timer = () => {
+    const [seconds, setSeconds] = useState(0);
+    console.log('Re-Rendering', seconds);
+    setInterval(() => {
+        setSeconds(seconds => seconds + 1)
+    }, 1000);
+
+    axios.get('').then(setPokemon());
+    return <h1>{seconds}</h1>
+}
+
+export default Timer;
