84a37aff609338f041d00bab523f09c10122b7bd
Coffee6ean <mejiapazalfredo@gmail.com>
2024-02-29 14:45:19 -0600

    Adding another custom hook to 'ReactJSFundamentals/CustomHooks' - useFetch

--- 22.ReactJSFundamentals/CustomHooks/App/seventh-app/src/App.js
+++ 22.ReactJSFundamentals/CustomHooks/App/seventh-app/src/App.js
@@ -3,10 +3,15 @@ import './App.css';
 import MoodClicker from './MoodClicker';
 import ColorPicker from './ColorPicker';
 import Counter from './Counter';
+import SignupForm from './SignupForm';
+import DogDetail from './DogDetail';
 
 function App() {
   return (
     <div className="App">
+      <SignupForm/>
+      <DogDetail/>
+      <hr/>
       <ColorPicker/>
       <Counter/>
       <MoodClicker/>
---
+++ 22.ReactJSFundamentals/CustomHooks/App/seventh-app/src/hooks/useFetch.js
@@ -0,0 +1,27 @@
+import React, {useState, useEffect} from "react";
+
+const useFetch = (url, options = {}) => {
+    const [response, setResponse] = useState(null);
+    const [error, setError] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+
+    // After the first render, fetch our data
+    useEffect(() => {
+        const fetchData = async() => {
+            try {
+                const res = await fetch(url);
+                const json = await res.json();
+                setResponse(json);
+            } catch(error) {
+                setError(error);
+            }
+
+            setIsLoading(false);
+        }
+        fetchData();
+    }, [url]);
+
+    return {response, error, isLoading};
+}
+
+export default useFetch;
