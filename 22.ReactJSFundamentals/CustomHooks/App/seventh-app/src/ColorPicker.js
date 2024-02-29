bc8e2b8feef738735517b48097d13f8d8d6d72b6
Coffee6ean <mejiapazalfredo@gmail.com>
2024-02-29 13:58:20 -0600

    Modified the localStorage hook to fit all elements

--- 22.ReactJSFundamentals/CustomHooks/App/seventh-app/src/ColorPicker.js
+++ 22.ReactJSFundamentals/CustomHooks/App/seventh-app/src/ColorPicker.js
@@ -2,23 +2,24 @@ import React from "react";
 import useLocalStorageState from "./hooks/useLocalStorageState";
 
 const ColorPicker = () => {
-    const [color, setColor] = useLocalStorageState('color', 'teal');
-    const changeColor = (e) => {
-        setColor(e.target.value);
-    }
-    return (
-        <>
-            <h1 style={{color}}>Your Color Is: {color}</h1>
-            <select value={color} onChange={changeColor}>
-                <option value="red">Red</option>
-                <option value="orange">Orange</option>
-                <option value="yellow">Yellow</option>
-                <option value="purple">Purple</option>
-                <option value="blue">Blue</option>
-                <option value="teal">Teal</option>
-            </select>
-        </>
-    )
-}
+  const [color, setColor] = useLocalStorageState('color', 'teal');
+  const changeColor = (e) => {
+    setColor(e.target.value);
+  };
+
+  return (
+    <>
+      <h1 style={{ color }}>Your Color Is: {color}</h1>
+      <select value={color} onChange={changeColor}>
+        <option value="red">Red</option>
+        <option value="orange">Orange</option>
+        <option value="yellow">Yellow</option>
+        <option value="purple">Purple</option>
+        <option value="blue">Blue</option>
+        <option value="teal">Teal</option>
+      </select>
+    </>
+  );
+};
 
 export default ColorPicker;
--- 22.ReactJSFundamentals/CustomHooks/App/seventh-app/src/hooks/useLocalStorageState.js
+++ 22.ReactJSFundamentals/CustomHooks/App/seventh-app/src/hooks/useLocalStorageState.js
@@ -1,16 +1,16 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
-const useLocalStorageState = (key, defaultValue) => {
-    const [state, setState] = useState(() => {
-        let value = window.localStorage.getItem(key) || defaultValue;
-        return value;
-    });
+const useLocalStorageState = (key, defaultValue, parseFunction = JSON.parse) => {
+  const [state, setState] = useState(() => {
+    const storedValue = parseFunction(window.localStorage.getItem(key) || defaultValue);
+    return storedValue;
+  });
 
-    useEffect(() => {
-        window.localStorage.setItem(key, state);
-    }, [key, state]);
+  useEffect(() => {
+    window.localStorage.setItem(key, JSON.stringify(state));
+  }, [key, state]);
 
-    return [state, setState];
-}
+  return [state, setState];
+};
 
 export default useLocalStorageState;
