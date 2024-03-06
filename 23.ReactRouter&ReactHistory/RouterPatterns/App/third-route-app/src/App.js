5c61c7672247eed2b4a8e2b88edf5616c4a1b88a
Coffee6ean <mejiapazalfredo@gmail.com>
2024-03-06 10:38:06 -0600

    Modified the React app in 'ReactRouter&ReactHistory/RouterPatterns/App/'- to fit navigation examples

--- 23.ReactRouter&ReactHistory/RouterPatterns/App/third-route-app/src/App.js
+++ 23.ReactRouter&ReactHistory/RouterPatterns/App/third-route-app/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import {BrowserRouter} from "react-router-dom";
+import Nav from "./Nav";
 import RoutesList from "./RouteList";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Nav/>
         <RoutesList/>
       </BrowserRouter>
     </div>
--- 23.ReactRouter&ReactHistory/RouterPatterns/App/third-route-app/src/BlogHome.js
+++ 23.ReactRouter&ReactHistory/RouterPatterns/App/third-route-app/src/BlogHome.js
@@ -1,5 +1,4 @@
 import React from "react";
-import {useParams} from "react-router-dom";
 import BaseGreetings from "./BaseGreetings";
 
 function BlogHome() {
