c8dae6e70256148603a086f0e9f5337da69cb6d8
Coffee6ean <mejiapazalfredo@gmail.com>
2024-03-06 12:37:55 -0600

    Modified react app in 'RouterPatterns/App/' - updated redirect method to fit library v6

--- 23.ReactRouter&ReactHistory/RouterPatterns/App/third-route-app/src/Contact.js
+++ 23.ReactRouter&ReactHistory/RouterPatterns/App/third-route-app/src/Contact.js
@@ -1,11 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import BaseGreetings from "./BaseGreetings";
+import { useNavigate } from "react-router-dom";
 
 function Contact() {
-    const val = 'Contact'
+    const val = 'Contact';
+    const [email, setEmail] = useState('');
+    const navigate = useNavigate();
+    console.log(navigate);
+
+    function handleChange(e) {
+        setEmail(e.target.value);
+    }
+
+    function storeEmail() {
+        alert('No email storage');
+    }
+
+    function handleSubmit(evt) {
+        evt.preventDefault();
+        storeEmail(email);
+        navigate('/');
+    }
+
     return (
         <div>
             <BaseGreetings name={val}/>
+            <p>To get in touch, enter email.</p>
+            <form onSubmit={handleSubmit}>
+                <input 
+                    type="email" 
+                    name="email" 
+                    value={email} 
+                    onChange={handleChange}
+                />
+                <button type="submit">Submit</button>
+            </form>
         </div>
     );
 }
